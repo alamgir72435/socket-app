8b33acb1a966e011ca4e28fc806e15923be72fb7
Alamgir Ahamed Saif <88821633+binarybunon@users.noreply.github.com>
2022-04-06 12:16:14 +0600

    commit

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ fs.readdir('./routes', (err, files) => {
 		});
 	});
 
-	const PORT = process.env.PORT || 5000;
+	const PORT = process.env.PORT || 10000;
 
 	const server = app.listen(PORT, () =>
 		console.log(`server Running on port ${PORT}`)
